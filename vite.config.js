2fe751726a22ffc106f88780c17c1e76ccecdbf1
Gadiel Us <gadyus18@gmail.com>
2023-08-15 16:51:06 -0600

    Creo que habia un error en la url de vite.config ahora veremos si responde

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins:[react()],
-    base:'https://giuc777.github.io/MarvelVerse'
+    base:'https://giuc777.github.io/MarvelVerse/'
 })
